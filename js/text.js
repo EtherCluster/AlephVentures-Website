9fd0f73aac108687bfdf54b862fdebbf9b621050
EtherCluster <ethercluster@protonmail.com>
2022-01-03 16:12:30 +0200

    new collection page

--- js/text.js
+++ js/text.js
@@ -21,3 +21,79 @@ function openGallery(id) {
   $(window).load(function(){
     $('#exampleModal').modal('show');
 });
+
+(function ($) {
+  "use strict";
+
+  // COLOR MODE
+  $(".color-mode").click(function () {
+    toggleLightAndDarkMode();
+  });
+
+  function toggleLightAndDarkMode() {
+    console.log(" CLICKED!!!");
+    $(".color-mode-icon").toggleClass("active");
+    $(".color-mode-text").toggleClass("active");
+    $("body").toggleClass("dark-mode");
+
+    const isDarkMode = $(".color-mode-icon").hasClass("active");
+    if (isDarkMode) {
+      console.log(" IS DARK");
+      localStorage.setItem("darkMode", 1);
+    } else {
+      console.log(" IS LIGHT");
+      localStorage.setItem("darkMode", 0);
+    }
+  }
+
+  // HEADER
+  $(".navbar").headroom();
+
+  // PROJECT CAROUSEL
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true,
+  });
+
+  // SMOOTHSCROLL
+  $(function () {
+    $(".nav-link, .custom-btn-link").on("click", function (event) {
+      var $anchor = $(this);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top - 49,
+          },
+          1000
+        );
+      event.preventDefault();
+    });
+  });
+
+  // TOOLTIP
+  $(".social-links a").tooltip();
+
+  console.log(" CUSTOM JS LOADED");
+
+  const selectedMode = localStorage.getItem("darkMode");
+  if (selectedMode == 1) {
+    console.log(" IS DARK");
+    $(".color-mode-icon").addClass("active");
+    $(".color-mode-text").addClass("active");
+    $("body").addClass("dark-mode");
+  } else {
+    console.log(" IS LIGHT");
+    $(".color-mode-icon").removeClass("active");
+    $(".color-mode-text").removeClass("active");
+    $("body").removeClass("dark-mode");
+  }
+})(jQuery);
+
+
+
+$(window).load(function(){
+  $('#exampleModal').modal('show');
+});
